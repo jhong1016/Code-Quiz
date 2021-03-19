99cf234e4dadb18bd7107505c0c28be4697b1dde
Jin Hong <jnhg1016@gmail.com>
2021-03-20 10:22:00 +1100

    questions added to script.jss

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        title: "What tag defines the body of the HTML document, and usually includes all the contents such as the text, hyperlinks, images, tables, lists, and more?",
+        choices: ["<head></head>", "<body></body>", "<title></title>", "<br>"],
+        answer: "<body></body>",
+    },
+    {
+        title: "Which of these is not used to loop?",
+        choices: ["for", "while", "foreach", "sequence"],
+        answer: "sequence",
+    },
+    {
+        title: "CSS stands for ____ Style Sheets.",
+        choices: ["Curious", "Concept", "Cascading", "Concave"],
+        answer: "Cascading",
+    },
+]
